8c18f16fbc0a59e0cef45451bd655f0fc93a053a
trevorlon211 <trevorlon.211@gmail.com>
2016-04-26 22:26:07 -0400

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417040540) do
+ActiveRecord::Schema.define(version: 20160427012902) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "string"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
