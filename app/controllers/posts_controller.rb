e7d6cbbf153069f5f4e77314f244bd6d69f438dc
trevorlon211 <trevorlon.211@gmail.com>
2016-04-27 22:05:18 -0400

    edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
-		@posts = Post.all.order('created_at DESC')
+		@post = Post.all.order('created_at DESC')
 	end
 
 	def new
 		@post = Post.new
 	end
 
-	def create
-      @post = Post.new(post_params)
-      
-      if @post.save
-         redirect_to @post
-      else
-      	render 'new'
-      end
-    end
-
-
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -41,12 +41,12 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 		@post.destroy
 
-		redirect_to root_path
+		redirect_to posts_path
 	end
 
-
 	private
-	 def post_params
-	   params.require(:post).permit(:title, :body)
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
