af1c32d1090de6f83feacf19e0a63573045b7525
trevorlon211 <trevorlon.211@gmail.com>
2016-04-27 23:18:10 -0400

    edits

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, User, :id => user.id
+      can :manage, User, :id => user.id
+    end
   end
-
 end
